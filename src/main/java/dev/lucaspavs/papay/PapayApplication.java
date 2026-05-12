84f65b4bee10eac5920129133830b5a7aeb2ef31
Lucas Pereira <lucaspereiraprooficial@gmail.com>
2026-05-12 03:21:36 -0300

    chore: initial project setup with spring boot and postgresql

---
+++ src/main/java/dev/lucaspavs/papay/PapayApplication.java
@@ -0,0 +1,13 @@
+package dev.lucaspavs.papay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lucaspavs/papay/PapayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucaspavs.papay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
